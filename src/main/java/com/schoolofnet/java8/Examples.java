fc97b75894f70d2914ce4ac25920d570ef316e9d
gustcoder <gustavofaramos@gmail.com>
2022-02-13 11:08:18 -0300

    Functional Interfaces

--- src/main/java/com/schoolofnet/java8/Examples.java
+++ src/main/java/com/schoolofnet/java8/Examples.java
@@ -23,5 +23,20 @@ public class Examples {
 
         // Java 8
         result.forEach(Person::say);
+
+        // Java 7
+        MyInterface myInterface = new MyInterface() {
+            @Override
+            public void print(String s) {
+                System.out.println("Java 7 implementation Functional Interface");
+            }
+        };
+        myInterface.print("Java 7");
+
+        // Java 8
+        MyInterface my1 = (String s) -> {
+            System.out.println("Java 8 implementation Functional Interface: " + s);
+        };
+        result.forEach(my1::print);
     }
 }
---
+++ src/main/java/com/schoolofnet/java8/MyInterface.java
@@ -0,0 +1,6 @@
+package com.schoolofnet.java8;
+
+@FunctionalInterface
+public interface MyInterface {
+    void print(String s);
+}
