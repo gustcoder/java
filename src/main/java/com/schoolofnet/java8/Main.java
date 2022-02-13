54fbe0804c3d67422f30a2f48dc7bf6abd6dda5a
gustcoder <gustavofaramos@gmail.com>
2022-02-13 00:17:48 -0300

    Implementing Lambda with Java 8

--- src/main/java/com/schoolofnet/java8/Main.java
+++ src/main/java/com/schoolofnet/java8/Main.java
@@ -2,6 +2,17 @@ package com.schoolofnet.java8;
 
 public class Main {
     public static void main(String... args) {
-        System.out.println("Java 8 - School of Net");
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Java 8 - School of Net");
+            }
+        };
+
+        new Thread(run).start();
+
+        // using Lambda
+        Runnable runWithLambda = () -> System.out.println("Java 8 With Lambda");
+        new Thread(runWithLambda).start();
     }
 }
