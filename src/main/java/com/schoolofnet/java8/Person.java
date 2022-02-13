69f1e0006140d04e231161ef747240322c17c8d9
gustcoder <gustavofaramos@gmail.com>
2022-02-13 11:44:41 -0300

    Optionals

--- src/main/java/com/schoolofnet/java8/Person.java
+++ src/main/java/com/schoolofnet/java8/Person.java
@@ -1,6 +1,21 @@
 package com.schoolofnet.java8;
 
 public class Person {
+    private String name;
+
+    public Person(String name) {
+        // construct
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public static void say(String str) {
         System.out.println("A person is saying something: " + str);
     }
